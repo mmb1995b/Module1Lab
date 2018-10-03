d76c2c2720f74559e316db682a1f8800ea8b20ec
Butler <butlemm1@vanderbilt.edu>
2018-10-03 15:18:03 -0500

    game exercise

--- Guessing Game/game.js
+++ Guessing Game/game.js
@@ -1,21 +1,29 @@
 // program data:
 var number = 22;
 var guess;
+var limit = 5;
+var won = false;
 
 
 
+for (i = 1; i <= limit; i++){
+    // prompt user for their guess:
+    guess = prompt("Please guess a number: ");
 
-// prompt user for their guess:
-guess = prompt("Please guess a number: ");
+    // if correct: let the user know they won
 
-// if correct: let the user know they won
-if (guess == number){
-    document.write("Congratulations, that guess is correct! You win!")
+        if (guess == number){
+            document.write("Congratulations, that guess is correct! You win!");
+            won = true;
+            break;
+        }
+
+        // if incorrect: let the user know
+        else{
+            alert("I'm sorry, that guess is incorrect. You have " + (limit - i) + " guesses remaining.");
+
+        }
+    }
+if (!won){
+    document.write("I'm sorry, you have no more guesses. Better luck next time.");
 }
-
-// if incorrect: let the user know
-else{
-    alert("I'm sorry, that guess is incorrect")
-
-}
-
